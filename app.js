c808c8bd1de412aea4c091120ea62c79baee26b3
Ken <chachgit21@gmail.com>
2023-02-05 12:11:49 -0500

    Players choice is printed in results section when clicked

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 let pressButtons = document.querySelectorAll('.choose')
-
+let printPlayer = document.getElementById('choiceofplayer')
 // I selected all the elements with the choose class. 
 
 pressButtons[0].onclick = function (infoclick) {
-    player = "rock"
+    player = "Rock"
     console.log(infoclick)
+    printPlayer.textContent = player 
+
 }
 
 // When I press the rock button. I want the player value to be set to rock. 
 
 pressButtons[1].onclick = function (infoclick) {
-    player = "paper"
+    player = "Paper"
+    printPlayer.textcontent=player
     
 }
 
 pressButtons[2].onclick = function (infoclick) {
-    player = "rock"
+    player = "Scissors"
+    printPlayer.textcontent=player
     
 }
 
 //  created buttons for paper and scissors. 
+
 
