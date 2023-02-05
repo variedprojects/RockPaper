e2d032691a58ec7260e4e0cda76d5b5fac172074
Ken <chachgit21@gmail.com>
2023-02-05 11:16:41 -0500

    two buttons added

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 let pressButtons = document.querySelectorAll('.choose')
 
+// I selected all the elements with the choose class. 
+
 pressButtons[0].onclick = function (infoclick) {
     player = "rock"
     console.log(infoclick)
 }
 
 // When I press the rock button. I want the player value to be set to rock. 
+
+pressButtons[1].onclick = function (infoclick) {
+    player = "paper"
+    
+}
+
+pressButtons[2].onclick = function (infoclick) {
+    player = "rock"
+    
+}
+
+//  created buttons for paper and scissors. 
+
