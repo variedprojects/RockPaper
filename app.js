53c8d5368db9b8793cf023301398387f400aae33
Ken <chachgit21@gmail.com>
2023-02-05 13:29:00 -0500

    readme created

--- app.js
+++ app.js
@@ -5,24 +5,28 @@ let printPlayer = document.getElementById('choiceofplayer')
 pressButtons[0].onclick = function (infoclick) {
     player = "Rock"
     console.log(infoclick)
-    printPlayer.textContent = player 
+    printPlayer.textContent = "👊" 
+    // This helps us print the players choice on the screen
 
 }
 
 // When I press the rock button. I want the player value to be set to rock. 
+// Whenever button is clicked players choice is printed.
 
 pressButtons[1].onclick = function (infoclick) {
     player = "Paper"
     printPlayer.textcontent=player
     
+    
 }
 
 pressButtons[2].onclick = function (infoclick) {
     player = "Scissors"
-    printPlayer.textcontent=player
+    printPlayer.textcontent="✌"
     
 }
 
 //  created buttons for paper and scissors. 
+// Whenever button is clicked players choice is printed.
 
 
